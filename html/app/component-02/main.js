740783be4fc48c243d6ab42f5491e50131057efd
Lyle Sigurdson <lyle@sent.com>
2015-10-06 11:49:50 -0500

    Made one of the components a google map

--- html/app/component-02/main.js
+++ html/app/component-02/main.js
@@ -1,12 +1,31 @@
+/*jshint browser: true */
 import ko from 'knockout';
 import template from './template.html!text';
 import {} from './style.css!';
+import mapsapi from 'google-maps-api';
+import config from 'app/app-config.json!';
 
 export default class {
     constructor(viewModel) {
         ko.components.register('component-02', {
             viewModel: { instance: viewModel },
             template: template
+        });
+
+        mapsapi(config.googleApiKey)().then(maps => {
+            let map = new maps.Map(document.getElementById('component-02--map'), {
+                center: { lat: -34.39, lng: 150.644 },
+                zoom: 8
+            });
+
+            let marker = new maps.Marker({
+                map: map,
+                position: { lat: -34.39, lng: 150.644 }
+            });
+
+            setInterval(() => {
+                marker.setVisible(!marker.getVisible());
+            }, 1000);
         });
     }
 }
--- html/config.js
+++ html/config.js
@@ -19,11 +19,28 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@1.2.1",
     "css": "github:systemjs/plugin-css@0.1.18",
+    "google-maps-api": "npm:google-maps-api@1.1.0",
+    "json": "github:systemjs/plugin-json@0.1.0",
     "knockout": "github:knockout/knockout@3.3.0",
     "radiosilence/xr": "github:radiosilence/xr@master",
     "text": "github:systemjs/plugin-text@0.0.2",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:asap@1.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.25": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -32,6 +49,27 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:google-maps-api@1.1.0": {
+      "latlng": "npm:latlng@1.0.0",
+      "promise": "npm:promise@6.1.0",
+      "scriptjs": "npm:scriptjs@2.5.8"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:promise@6.1.0": {
+      "asap": "npm:asap@1.0.0"
+    },
+    "npm:scriptjs@2.5.8": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
