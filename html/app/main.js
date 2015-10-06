028d5c32371194c84e83351ed5d5b9918563f3fb
Lyle Sigurdson <lyle@sent.com>
2015-10-05 20:12:37 -0500

    ViewModel class
    ViewModel class with init and update and ready for any more behaviors
    it might need.

--- html/app/main.js
+++ html/app/main.js
@@ -6,6 +6,51 @@ import koMapping from 'SteveSanderson/knockout.mapping';
 (function () {
     'use strict';
 
+    let getCategories = (venues) => {
+        let categories = new Map(),
+            result = [];
+
+        venues.forEach(venue =>
+            venue.categories.forEach(category =>
+                categories.set(category.id, category)
+            )
+        );
+
+        categories.forEach((value) =>
+            result.push(value)
+        );
+
+        return {
+            categories: result
+        };
+    };
+
+    let mapping = {
+        venues: {
+            key: data => ko.utils.unwrapObservable(data.id),
+            create: options => new Venue(options.data)
+        },
+        categories: {
+            key: data => ko.utils.unwrapObservable(data.id),
+            create: options => new Category(options.data)
+        }
+    };
+
+    class ViewModel {
+        constructor() {
+        }
+
+        init(data) {
+            koMapping.fromJS(data, mapping, this);
+            koMapping.fromJS(getCategories(data.venues), mapping, this);
+        }
+
+        update(data) {
+            koMapping.fromJS(data, this);
+            koMapping.fromJS(getCategories(data.venues), this);
+        }
+    }
+
     class Venue {
         constructor(spec) {
             this.id = spec.id;
@@ -23,54 +68,20 @@ import koMapping from 'SteveSanderson/knockout.mapping';
         }
     }
 
-    let mapping = {
-        venues: {
-            key: data => ko.utils.unwrapObservable(data.id),
-            create: options => new Venue(options.data)
-        },
-        categories: {
-            key: data => ko.utils.unwrapObservable(data.id),
-            create: options => new Category(options.data)
-        }
-    };
 
-    let getCategories = (venues) => {
-        let categories = new Map(),
-            result = [];
-
-        venues.forEach(venue =>
-            venue.categories.forEach(category =>
-                categories.set(category.id, category)
-            )
-        );
-
-        categories.forEach((value) =>
-            result.push(value)
-        );
-
-        return result;
-    };
+    // Main
+    let viewModel = new ViewModel();
 
     xr.get('test-data.json')
         .then(function (result) {
-            let data = {
-                venues: result.response.venues,
-                categories: getCategories(result.response.venues)
-            };
-
-            let viewModel = koMapping.fromJS(data, mapping);
+            viewModel.init(result.response);
 
             ko.applyBindings(viewModel);
 
             [ 'test-data-one.json', '123-main.json', 'test-data.json' ].forEach((file, index) => {
                 setTimeout(() => {
-                    console.log(index);
-                    xr.get(file).then(result => {
-                        let data = {
-                            venues: result.response.venues,
-                            categories: getCategories(result.response.venues)
-                        };
-                        koMapping.fromJS(data, viewModel);
+                    xr.get(file).then(data => {
+                        viewModel.update(data.response);
                     });
                 }, (index + 1) * 5000);
             });
