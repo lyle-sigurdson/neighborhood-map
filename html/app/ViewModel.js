6a233101dcaf2b303735c1f7d1ac6d224ff1896d
Lyle Sigurdson <lyle@sent.com>
2015-10-07 09:41:55 -0500

    Make ViewModel its own module

---
+++ html/app/ViewModel.js
@@ -0,0 +1,63 @@
+/*jshint module: true */
+import ko from 'knockout';
+import koMapping from 'SteveSanderson/knockout.mapping';
+
+class Venue {
+    constructor(spec) {
+        this.id = spec.id;
+        this.name = spec.name;
+        this.location = spec.location;
+        this.categories = spec.categories;
+        this.visible = ko.observable(true);
+    }
+}
+
+class Category {
+    constructor(spec) {
+        this.id = spec.id;
+        this.pluralName = spec.pluralName;
+    }
+}
+
+let getCategories = (venues) => {
+    let categories = new Map();
+
+    venues.forEach(venue =>
+        venue.categories.forEach(category =>
+            categories.set(category.id, category)
+        )
+    );
+
+    let result = [];
+
+    categories.forEach((value) =>
+        result.push(value)
+    );
+
+    return {
+        categories: result
+    };
+};
+
+let mapping = {
+    venues: {
+        key: data => ko.utils.unwrapObservable(data.id),
+        create: options => new Venue(options.data)
+    },
+    categories: {
+        key: data => ko.utils.unwrapObservable(data.id),
+        create: options => new Category(options.data)
+    }
+};
+
+export default class ViewModel {
+    constructor(data) {
+        koMapping.fromJS(data, mapping, this);
+        koMapping.fromJS(getCategories(data.venues), mapping, this);
+    }
+
+    update(data) {
+        koMapping.fromJS(data, this);
+        koMapping.fromJS(getCategories(data.venues), this);
+    }
+}
--- html/app/main.js
+++ html/app/main.js
@@ -1,77 +1,13 @@
 /*jshint browser: true, devel: true */
 import xr from 'radiosilence/xr';
-import ko from 'knockout';
-import koMapping from 'SteveSanderson/knockout.mapping';
+
+import ViewModel from './ViewModel';
 
 import Component01 from 'app/components/component-01/main';
 import VenuesMap from 'app/components/venues-map/main';
 
 (function () {
     'use strict';
-
-    let getCategories = (venues) => {
-        let categories = new Map();
-
-        venues.forEach(venue =>
-            venue.categories.forEach(category =>
-                categories.set(category.id, category)
-            )
-        );
-
-        let result = [];
-
-        categories.forEach((value) =>
-            result.push(value)
-        );
-
-        return {
-            categories: result
-        };
-    };
-
-    let mapping = {
-        venues: {
-            key: data => ko.utils.unwrapObservable(data.id),
-            create: options => new Venue(options.data)
-        },
-        categories: {
-            key: data => ko.utils.unwrapObservable(data.id),
-            create: options => new Category(options.data)
-        }
-    };
-
-    class ViewModel {
-        constructor(data) {
-            koMapping.fromJS(data, mapping, this);
-            koMapping.fromJS(getCategories(data.venues), mapping, this);
-        }
-
-        update(data) {
-            koMapping.fromJS(data, this);
-            koMapping.fromJS(getCategories(data.venues), this);
-        }
-    }
-
-    class Venue {
-        constructor(spec) {
-            this.id = spec.id;
-            this.name = spec.name;
-            this.location = spec.location;
-            this.categories = spec.categories;
-            this.visible = ko.observable(true);
-        }
-    }
-
-    class Category {
-        constructor(spec) {
-            this.id = spec.id;
-            this.pluralName = spec.pluralName;
-        }
-    }
-
-
-    // Main
-
     xr.get('test-data.json')
         .then(function (result) {
             // Init the app.
