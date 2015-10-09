01fcee7d60039e7e0324ff43eb97f848d2141b7a
Lyle Sigurdson <lyle@sent.com>
2015-10-09 09:08:05 -0500

    Add function to get venues from foursquare.com

---
+++ html/app/getVenues.js
@@ -0,0 +1,15 @@
+import xr from 'radiosilence/xr';
+
+export default function (ll) {
+    // Foursquare API version.
+    const v = '20151008';
+
+    return xr.get(`/foursquare-venues?ll=${ll}&v=${v}`).then(res => {
+        if (res.meta.code !== 200) {
+            throw new Error(`Foursquare: code ${res.meta.code}, ` +
+                `errorType ${res.meta.errorType}`
+            );
+        }
+        return res.response;
+    });
+}
