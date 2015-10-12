71e63ac8c4a0bb5cfa0c358410ac2fa038d19dbe
Lyle Sigurdson <lyle@sent.com>
2015-10-12 16:43:24 -0500

    Added mechanism for storing preferences

--- html/app/App.js
+++ html/app/App.js
@@ -7,8 +7,16 @@ import VenuesMap from './components/venues-map/main';
 import getVenues from './getVenues';
 import getIpinfo from './getIpinfo';
 
+import config from './app-config.json!';
+
 export default class {
     constructor() {
+        if (!window.localStorage.getItem('preferences')) {
+            window.localStorage.setItem(
+                'preferences', JSON.stringify(config.defaultPreferences)
+            );
+        }
+
         this.viewModel = new ViewModel();
         this.component01 = new Component01(this.viewModel);
         this.venuesMap = new VenuesMap(this.viewModel);
@@ -26,10 +34,12 @@ export default class {
     }
 
     run() {
-        this.venuesMap.addEventListener('dragend', e => {
-            return getVenues(e).then(venues => {
-                this.viewModel.update(venues);
+        if (JSON.parse(window.localStorage.getItem('preferences')).uiMode === 'drag') {
+            this.venuesMap.addEventListener('dragend', e => {
+                return getVenues(e).then(venues => {
+                    this.viewModel.update(venues);
+                });
             });
-        });
+        }
     }
 }
