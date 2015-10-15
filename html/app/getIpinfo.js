f1b3547cd170907e4fa4c61ec8890365fe2746f8
Lyle Sigurdson <lyle@sent.com>
2015-10-15 15:07:22 -0500

    Check for errors from api proxy

--- html/app/getIpinfo.js
+++ html/app/getIpinfo.js
@@ -2,10 +2,14 @@ import xr from 'radiosilence/xr';
 
 export default function () {
     return xr.get('/ipinfo/').then(ipinfo => {
-        console.log('ipinfo', ipinfo);
+        if (ipinfo.apiProxyError) {
+            throw new Error('getIpinfo: apiProxy: ' + JSON.stringify(ipinfo.apiProxyError));
+        }
+
         if (ipinfo.hasOwnProperty('bogon')) {
             throw new Error(`ipinfo.io: bogon ${ipinfo.ip} ${ipinfo.hostname}`);
         }
+
         return ipinfo;
     });
 }
--- html/app/getVenues.js
+++ html/app/getVenues.js
@@ -5,11 +5,16 @@ export default function (ll) {
     const v = '20151008';
 
     return xr.get(`/foursquare-venues?ll=${ll}&v=${v}`).then(res => {
+        if (res.apiProxyError) {
+            throw new Error('getVenues: apiProxy: ' + JSON.stringify(res.apiProxyError));
+        }
+
         if (res.meta.code !== 200) {
             throw new Error(`Foursquare: code ${res.meta.code}, ` +
                 `errorType ${res.meta.errorType}`
             );
         }
+
         return res.response;
     });
 }
