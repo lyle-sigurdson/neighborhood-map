066723518fc29a3db4db6d6986ef62ac43f502b2
Lyle Sigurdson <lyle@sent.com>
2015-10-16 14:25:53 -0500

    Better error message from getVenues function

--- html/app/getVenues.js
+++ html/app/getVenues.js
@@ -2,16 +2,17 @@ import xr from 'radiosilence/xr';
 
 export default function (ll) {
     // Foursquare API version.
-    const v = '20151008';
+    const v = '20151008',
+          query = `/foursquare-venues?ll=${ll}&v=${v}`;
 
-    return xr.get(`/foursquare-venues?ll=${ll}&v=${v}`).then(res => {
+    return xr.get(query).then(res => {
         if (res.apiProxyError) {
             throw new Error('getVenues: apiProxy: ' + JSON.stringify(res.apiProxyError));
         }
 
         if (res.meta.code !== 200) {
             throw new Error(`Foursquare: code ${res.meta.code}, ` +
-                `errorType ${res.meta.errorType}`
+                `errorType ${res.meta.errorType}. Query was ${query}`
             );
         }
 
