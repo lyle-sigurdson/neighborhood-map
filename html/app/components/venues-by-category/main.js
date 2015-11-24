3d8abd1c9ca5a6e945051330642e91ade279966f
Lyle Sigurdson <lyle@sent.com>
2015-11-24 12:51:37 -0600

    Scroll category list to top when list is refreshed
    When a new batch of categories of venues is displayed in the list,
    scroll the list to the top instead of leaving it where it was last
    scrolled to.

--- html/app/components/venues-by-category/main.js
+++ html/app/components/venues-by-category/main.js
@@ -1,8 +1,19 @@
+/*jshint browser: true */
 import template from './template.html!text';
 
 class ViewModel {
     constructor(params) {
         this.categories = params.categories;
+
+        let element$ = window.document.getElementsByClassName(
+            'c-venues-by-category'
+        );
+
+        this.categories.subscribe(() => {
+            Array.prototype.forEach.call(element$, element => {
+                element.scrollTop = 0;
+            });
+        });
     }
 }
 
